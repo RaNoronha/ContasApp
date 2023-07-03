be9e20c6cc3895e3b4a3aff25b889926a074d095
RaNoronha <ranlcgnn@gmail.com>
2023-07-03 10:54:31 -0300

    Feat: Criação do Controller "Account" e das views "Login", "Register" e "ForgotPassword"

---
+++ ContasApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContasApp.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Método da view Login
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Método da view Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Método da view ForgotPassword
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
