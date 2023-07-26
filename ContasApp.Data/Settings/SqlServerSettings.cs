27555518ea592d687819dbf334b06ca64dd815d4
RaNoronha <rancgnn@gmail.com>
2023-07-25 21:57:46 -0300

    conexao com myasp.net

--- ContasApp.Data/Settings/SqlServerSettings.cs
+++ ContasApp.Data/Settings/SqlServerSettings.cs
@@ -10,7 +10,8 @@ namespace ContasApp.Data.Settings
     {
         public static string ConnectionSql()
         {
-            return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDContasApp;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+            //return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDContasApp;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+            return "Data Source=SQL8005.site4now.net;Initial Catalog=db_a9cbcf_contasapp;User Id=db_a9cbcf_contasapp_admin;Password=Lz5365@pq";
         }
     }
 }
